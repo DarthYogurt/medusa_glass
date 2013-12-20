0eb22908884c3fe969df37d8e2a1d2d7fc5f0333
Deric Walintukan <deric.walintukan@gmail.com>
2013-12-19 20:17:01 -0800

    added base files

--- gen/com/medusa/checkit/R.java
+++ gen/com/medusa/checkit/R.java
@@ -12,22 +12,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_stop=0x7f020001;
+    }
+    public static final class id {
+        public static final int stop=0x7f070000;
+    }
+    public static final class menu {
+        public static final int checklist=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int stop=0x7f040002;
         public static final int voice_trigger_start_program=0x7f040001;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -39,11 +38,8 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppBaseTheme=0x7f050001;
+        public static final int MenuTheme=0x7f050000;
     }
     public static final class xml {
         public static final int voice_trigger_start=0x7f030000;
---
+++ src/com/medusa/checkit/MenuActivity.java
@@ -0,0 +1,49 @@
+package com.medusa.checkit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+
+// Show options menu
+public class MenuActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+	
+	@Override
+    public void onResume() {
+        super.onResume();
+        openOptionsMenu();
+    }
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.checklist, menu);
+        return true;
+    }
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+        switch (item.getItemId()) {
+            case R.id.stop:
+                stopService(new Intent(this, ChecklistService.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+	
+	@Override
+    public void onOptionsMenuClosed(Menu menu) {
+        // Nothing else to do, closing the Activity.
+        finish();
+    }
+}
