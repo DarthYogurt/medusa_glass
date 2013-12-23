5fffa22e3ab1bdcca0c4d79cc4befd4efbfc2aca
Deric Walintukan <deric.walintukan@gmail.com>
2013-12-22 18:33:32 -0800

    moved jsonreader into separate class

--- src/com/medusa/checkit/NewChecklistActivity.java
+++ src/com/medusa/checkit/NewChecklistActivity.java
@@ -25,7 +25,6 @@ import android.view.MotionEvent;
 public class NewChecklistActivity extends Activity {
 	
 	private GestureDetector mGestureDetector;
-	ArrayList<String[]> data;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -115,43 +114,5 @@ public class NewChecklistActivity extends Activity {
         return false;
     }
 
-    // Reads JSON file and puts all info into ArrayList<String[]>
-	void readJson() {
-		InputStream inputStream = getResources().openRawResource(R.raw.list_of_checklists);
 
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        int ctr;
-        try {
-            ctr = inputStream.read();
-            while (ctr != -1) {
-                byteArrayOutputStream.write(ctr);
-                ctr = inputStream.read();
-            }
-            inputStream.close();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        try {
-        	// Parse the data into JSONObject to get original data in form of JSON
-            JSONObject jObject = new JSONObject(byteArrayOutputStream.toString());
-
-            JSONArray jArray = jObject.getJSONArray("listOfChecklists");
-            String id="";
-            String checklistName ="";
-
-            data = new ArrayList<String[]>();
-            for (int i = 0; i < jArray.length(); i++) {
-                id = jArray.getJSONObject(i).getString("id");
-                checklistName = jArray.getJSONObject(i).getString("checklistName");
-                
-                Log.v("ID", id);
-                Log.v("Checklist Name", checklistName);
-                data.add(new String[] {id, checklistName});
-                System.out.println(Arrays.toString(data.get(i)));
-            }
-        } catch (Exception e2) {
-            e2.printStackTrace();
-        }
-    }
 }
